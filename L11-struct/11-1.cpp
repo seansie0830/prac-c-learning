0d77bdb598cf51d8e66f3e08b90c45ff7d5453d6
seansie <seansie@gmail.com>
2022-02-07 14:02:42 +0800

    test ignore

--- L11-struct/11-1.cpp
+++ L11-struct/11-1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<cstdlib>
 using namespace std;
 int main(){
     struct member{
@@ -17,20 +18,30 @@ int main(){
     cout<<"making struct successfully"<<endl;
     for(i=0;i<amount;i++){
         cout<<"#"<<i<<endl;
-        cout<<"What does #"<<i<<"member's name"<<endl;
+        cout<<" What does #"<<i<<" member's name"<<endl;
         cin>>list[i].name;
-        cout<<"What does #"<<i<<"member's age"<<endl;
+        cout<<" What does #"<<i<<" member's age"<<endl;
         cin>>list[i].age;
-        cout<<"What does #"<<i<<"member's height"<<endl;
+        cout<<" What does #"<<i<<" member's height"<<endl;
         cin>>list[i].height;
-        cout<<"What does #"<<i<<"member's id(10char)"<<endl;
+        cout<<" What does #"<<i<<" member's id(10char)"<<endl;
         cin>>list[i].id;
-        cout<<"if #"<<i<<"member is married (y/n)"<<endl;
+        cout<<" if #"<<i<<" member is married (y/n)"<<endl;
         cin>>temp;
         if(temp=='y') list[i].married=true;
         else list[i].married=false;
     }
+    system("cls");
     cout<<"Input successfully"<<endl;
+    cout<<"starting loading data"<<endl;
+    for(i=0;i<amount;i++){
+        cout<<"#"<<i<<endl;
+        cout<<" name: "<<list[i].name<<endl;
+        cout<<" age: "<<list[i].age<<endl;
+        cout<<" height: "<<list[i].height<<endl;
+        cout<<" id: "<<list[i].id<<endl;
+        cout<<" married: "<<list[i].married<<endl;
+    }
     cout<<endl;
     return 0;
 }
